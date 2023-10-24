98a77987604f8d211c3e42066860edd0e624c16d
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-10-24 20:17:36 +0400

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,85 @@
 import React, { Component } from 'react';
-import FeedbackOptions from "components/FeedbackOptions/FeedbackOptions";
-import Statistics from "components/Statistics/Statistics";
-import Section from "components/Section/Section";
-import Notification from './Notification/Notification'
+import { nanoid } from 'nanoid';
+import PhoneBook from './PhoneBook/PhoneBook';
+import Contacts from './Contacts/Contacts';
+import Filter from './Filter/Filter';
+
 export class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  handleFeedback = (option) => {
-    this.setState((prevState) => ({
-      [option]: prevState[option] + 1,
+  handleAddContact = contactData => {
+    const hasDuplicates = this.state.contacts.some(
+      contact => contact.name.toLowerCase() === contactData.name.toLowerCase()
+    );
+
+    if (hasDuplicates) {
+      alert(`Contact with the name ${contactData.name} already exists.`);
+      return;
+    }
+
+    const finalContact = {
+      ...contactData,
+      id: nanoid(),
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, finalContact],
     }));
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
+  handleDeleteContact = id => {
+    this.setState(prevState => {
+      const updatedContacts = prevState.contacts.filter(
+        contact => contact.id !== id
+      );
+      return {
+        contacts: updatedContacts,
+      };
+    });
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    if (total === 0) {
-      return 0;
-    }
-    return Math.round((this.state.good / total) * 100);
+  handleFilterChange = filter => {
+    this.setState({
+      filter: filter,
+    });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
   };
 
   render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    const options = Object.keys(this.state);
+    const { filter } = this.state;
+    const filteredContacts = this.getFilteredContacts();
 
     return (
       <div>
-        <Section title="Please leave feedback">
-          {options.length === 0 ? (
-            <p className="noFeedback">There is no feedback</p>
-          ) : (
-            <FeedbackOptions
-              options={options}
-              onLeaveFeedback={this.handleFeedback}
-            />
-          )}
-        </Section>
-
-        {total > 0 ? (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
+        <h1>Phonebook</h1>
+        <PhoneBook handleAddContact={this.handleAddContact} />
+        <h2>Contacts</h2>
+        <Filter filter={filter} handleFilterChange={this.handleFilterChange} />
+        <Contacts
+          contacts={filteredContacts}
+          handleDeleteContact={this.handleDeleteContact}
+        />
       </div>
     );
   }
 }
+
+
+  // componentDidMount() {
+  //   const stringifiedContacts =
+
+  // }
